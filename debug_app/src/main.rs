9013cf4dcb3dbf438733a8eb10ad3803a70e3042
Matheus Silveira Venturini <42521920+msventurini@users.noreply.github.com>
2025-08-15 04:51:30 -0300

    Added basic files.

--- debug_app/src/main.rs
+++
@@ -1,7 +0,0 @@
-
-// use egui_components::view_template::app_root;
-use iced_components::view_template::app_root;
-
-fn main() -> eframe::Result {
-    app_root()
-}
--- iced_components/src/lib.rs
+++ iced_components/src/lib.rs
@@ -1,48 +1,48 @@
-use iced::Center;
-use iced::widget::{Column ,button, column, text};
+// use iced::Center;
+// use iced::widget::{Column ,button, column, text};
 
-pub mod view_template {
-    pub fn app_root() -> iced::Result {}
-}
+// pub mod view_template {
+//     pub fn app_root() -> iced::Result {}
+// }
 
-pub fn app_root() -> iced::Result {
-    iced::run("My App",Counter::update, Counter::view)
-}
-
-// pub fn main() -> iced::Result {
+// pub fn app_root() -> iced::Result {
 //     iced::run("My App",Counter::update, Counter::view)
 // }
 
-#[derive(Default)]
-struct Counter {
-    value: i64
-}
+// // pub fn main() -> iced::Result {
+// //     iced::run("My App",Counter::update, Counter::view)
+// // }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    Increment,
-    Decrement
-}
+// #[derive(Default)]
+// struct Counter {
+//     value: i64
+// }
 
-impl Counter {
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Increment => {
-                self.value += 1;
-            }
-            Message::Decrement => {
-                self.value -= 1;
-            }
-        }
-    }
+// #[derive(Debug, Clone, Copy)]
+// enum Message {
+//     Increment,
+//     Decrement
+// }
 
-    fn view(&self) -> Column<'_, Message> {
-        column![
-            button("Increment").on_press(Message::Increment),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::Decrement)
-        ]
-        .padding(20)
-        .align_x(Center)
-    }
-}
+// impl Counter {
+//     fn update(&mut self, message: Message) {
+//         match message {
+//             Message::Increment => {
+//                 self.value += 1;
+//             }
+//             Message::Decrement => {
+//                 self.value -= 1;
+//             }
+//         }
+//     }
+
+//     fn view(&self) -> Column<'_, Message> {
+//         column![
+//             button("Increment").on_press(Message::Increment),
+//             text(self.value).size(50),
+//             button("Decrement").on_press(Message::Decrement)
+//         ]
+//         .padding(20)
+//         .align_x(Center)
+//     }
+// }
